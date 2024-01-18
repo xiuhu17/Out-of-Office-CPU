13b920272bdfbc3de305485698850ef5ce6294f0
xiuhu17 <mizhou17@yahoo.com>
2024-01-18 15:12:40 +0800

    update

--- mp_verif/sv_refresher/hdl/lfsr.sv
+++ mp_verif/sv_refresher/hdl/lfsr.sv
@@ -9,5 +9,15 @@ module lfsr #(
 );
 
   // TODO: Fill this out!
+  always_ff @( posedge clk ) begin 
+    if (rst) begin 
+      shift_reg <= SEED_VALUE;
+    end else begin 
+      if (en) begin 
+        shift_reg <= {{shift_reg[0] ^ shift_reg[2] ^ shift_reg[3] ^ shift_reg[5]}, {shift_reg[15:1]}};
+        rand_bit <= shift_reg[0];
+      end
+    end
+  end
 
 endmodule : lfsr
