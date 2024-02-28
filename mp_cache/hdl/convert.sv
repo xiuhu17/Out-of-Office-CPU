11c4de08561ab9a0825329628c6ae38c099435ed
xiuhu17 <mizhou17@yahoo.com>
2024-02-28 10:40:00 -0600

    update

--- mp_cache/hdl/convert.sv
+++ mp_cache/hdl/convert.sv
@@ -1,11 +1,39 @@
-module CONVERT(
-    input logic [255:0] din,
-    input logic [4:0] low_bit,
-    input logic [3:0] mask,
-    output logic [31:0] dout
+module CONVERT_READ(
+    input logic [255:0] din_256,
+    input logic [31:0] ufp_addr,
+    input logic [3:0] rmask,
+    output logic [31:0] dout_32
 );
 
-    logic [63:0] four_byte_group;
-    assign four_byte_group = din[32 * low_bit[4:2] +: 32];
-    assign dout = {8{mask[3]}, 8{mask[2]}, 8{mask[1]}, 8{mask[0]}} & four_byte_group;
+    logic [31:0] four_byte_group;
+    always_comb begin
+        four_byte_group = din_256[32 * ufp_addr[4:2] +: 32];
+        dout_32 = {8{rmask[3]}, 8{rmask[2]}, 8{rmask[1]}, 8{rmask[0]}} & four_byte_group;
+    end
+endmodule
+
+module CONVERT_WRITE(
+    input logic [31:0] din_32,
+    input logic [255:0] din_256,
+    input logic [31:0] ufp_addr,
+    input logic [3:0] wmask,
+    output logic [255:0] dout_256
+);
+
+    always_comb begin 
+        dout_256 = din_256;
+        if (wmask[0]) begin 
+            dout_256[32 * ufp_addr[4:2] +: 8] = din_32[7 : 0];
+        end
+        if (wmask[1]) begin 
+            dout_256[8 + 32 * ufp_addr[4:2] +: 8] = din_32[15 : 8];
+        end
+        if (wmask[2]) begin 
+            dout_256[16 + 32 * ufp_addr[4:2] +: 8] = din_32[23 : 16];
+        end
+        if (wmask[3]) begin 
+            dout_256[24 + 32 * ufp_addr[4:2] +: 8] = din_32[31 : 24];
+        end
+    end 
+
 endmodule
---
+++ mp_cache/hdl/misshit.sv
@@ -0,0 +1,17 @@
+// [way 3][way 2][way 1][way 0]
+module HITMISS(
+    input logic [23:0] dirty_tag_0,
+    input logic [23:0] dirty_tag_1,
+    input logic [23:0] dirty_tag_2,
+    input logic [23:0] dirty_tag_3,
+    input logic valid_0,
+    input logic valid_1,
+    input logic valid_2,
+    input logic valid_3,
+    input logic [23:0] curr_tag,
+    output logic [3:0] way_mask,
+    output logic hit
+);
+
+
+endmodule
