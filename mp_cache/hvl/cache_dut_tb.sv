ce20cf175aee9661449bbda28da53211d84c9de7
xiuhu17 <mizhou17@yahoo.com>
2024-03-06 19:43:27 -0600

    update

--- mp_cache/hvl/cache_dut_tb.sv
+++ mp_cache/hvl/cache_dut_tb.sv
@@ -92,8 +92,8 @@ module cache_dut_tb;
     //---------------------------------------------------------------------------------
     // TODO: Write tasks to test various functionalities:
     //---------------------------------------------------------------------------------
-   task automatic test_read_hit(logic [31:0] addr, logic [31:0] expect_data, int line);
- PASSED = 1'b1;
+task automatic test_hit_read(logic [31:0] addr, logic [31:0] expect_data, int line);
+    PASSED = 1'b1;
     if (addr[1:0] != 2'b00) $fatal("%0t %s %0d: Addr not aligned", $time, `__FILE__, line);
     ufp_addr = addr;
     ufp_rmask = 4'b1111;
@@ -120,17 +120,14 @@ module cache_dut_tb;
     display_colored(" Should not write to mem", "red");
      $fatal("%0t %s %0d: Should not write to mem", $time, `__FILE__, line);
      end
-    ufp_rmask = '0;
     
     if (PASSED) display_colored("[PASSED] CACHE", "green");
     else display_colored("[FAILED] CACHE", "red");
     
-     @(posedge clk);
     ufp_rmask = '0;
- 
 endtask
 
-task automatic test_read_miss(logic [31:0] addr, logic [31:0] expect_data, int line);
+task automatic test_clean_miss_read(logic [31:0] addr, logic [31:0] expect_data, int line);
     PASSED = 1'b1;
     
     if (addr[1:0] != 2'b00) begin
@@ -171,9 +168,223 @@ task automatic test_read_miss(logic [31:0] addr, logic [31:0] expect_data, int l
     $fatal("%0t %s %0d: Data error", $time, `__FILE__, line, "red");
     
     end
-    ufp_rmask = '0;
+    
     if (PASSED) display_colored("[PASSED] CACHE", "green");
     else display_colored("[FAILED] ALU", "red");
+
+    ufp_rmask = '0;
+endtask
+
+task automatic test_clean_miss_write(logic [31:0] addr, logic [31:0] wdata, int line);
+    PASSED = 1'b1;
+   
+    if (addr[1:0] != 2'b00) begin
+    PASSED = 1'b0;
+    display_colored(" Addr not aligned", "red");
+    $fatal("%0t %s %0d: Addr not aligned", $time, `__FILE__, line, "red");
+    end
+   
+    ufp_addr = addr;
+    ufp_wmask = 4'b1111;
+    ufp_rmask = '0;
+    ufp_wdata = wdata;
+    @(posedge clk);
+    if (ufp_resp) begin
+    PASSED = 1'b0;
+    display_colored("Should not hit", "red");
+    $fatal("%0t %s %0d: Should not hit", $time, `__FILE__, line, "red");
+     end
+    @(posedge clk);
+    @(negedge clk);
+   
+    if (~mem_itf.read) begin
+    PASSED = 1'b0;
+    display_colored("Not reading from mem", "red");
+     $fatal("%0t %s %0d: Not reading from mem", $time, `__FILE__, line, "red");  
+    end
+    if (mem_itf.write) begin
+    display_colored("Read/write mem at the same time", "red");
+     $fatal("%0t %s %0d: Read/write mem at the same time", $time, `__FILE__, line, "red");
+     
+     end
+    begin
+    @(posedge clk iff mem_itf.resp);
+    end
+    repeat (2) @(posedge clk);
+    if (!ufp_resp) begin
+        PASSED = 1'b0;
+        display_colored("Write should have hit now man", "red");
+        $fatal("%0t %s %0d: Write should have hit now man", $time, `__FILE__, line, "red");
+    end
+
+    if (PASSED) display_colored("[PASSED] CACHE", "green");
+    else display_colored("[FAILED] ALU", "red");
+
+    ufp_wmask = '0;
+    ufp_wdata  = '0;
+endtask
+
+task automatic test_dirty_miss_read(logic [31:0] addr, logic [31:0] expect_data, int line);
+    PASSED = 1'b1;
+    
+    if (addr[1:0] != 2'b00) begin
+    PASSED = 1'b0;
+    display_colored(" Addr not aligned", "red");
+    $fatal("%0t %s %0d: Addr not aligned", $time, `__FILE__, line, "red");
+    end
+    
+    ufp_addr = addr;
+    ufp_rmask = 4'b1111;
+    ufp_wmask = 4'b0000;
+    @(posedge clk);
+    if (ufp_resp) begin
+    PASSED = 1'b0;
+    display_colored("Should not hit", "red");
+    $fatal("%0t %s %0d: Should not hit", $time, `__FILE__, line, "red");
+     end
+    @(posedge clk);
+    @(negedge clk);
+    if (~mem_itf.write) begin 
+        PASSED = 1'b0;
+        display_colored("Should start write to main mem", "red");
+        $fatal("%0t %s %0d: Should start write to main mem", $time, `__FILE__, line, "red");
+    end 
+    if (mem_itf.read) begin 
+        PASSED = 1'b0;
+        display_colored("Should not read from main mem", "red");
+        $fatal("%0t %s %0d: Should not read from main mem", $time, `__FILE__, line, "red");
+    end 
+    begin 
+    @(posedge clk iff mem_itf.resp);
+    end
+    @(posedge clk);
+    if (~mem_itf.read) begin 
+        PASSED = 1'b0;
+        display_colored("Should start read from main mem", "red");
+        $fatal("%0t %s %0d: Should start read from main mem", $time, `__FILE__, line, "red");
+    end 
+    if (mem_itf.write) begin 
+        PASSED = 1'b0;
+        display_colored("Should not write to main mem", "red");
+        $fatal("%0t %s %0d: Should start write to main mem", $time, `__FILE__, line, "red");
+    end
+    begin 
+    @(posedge clk iff mem_itf.resp);
+    end
+    repeat (2) @(posedge clk);
+    if (~ufp_resp) begin 
+        PASSED = 1'b0;
+        display_colored("Should report to cpu", "red");
+        $fatal("%0t %s %0d: Should report to cpu", $time, `__FILE__, line, "red");
+    end 
+    if (ufp_rdata !== expect_data) begin
+        PASSED = 1'b0; 
+        display_colored("Data error", "red");
+        $fatal("%0t %s %0d: Data error", $time, `__FILE__, line, "red");
+    end
+
+    if (PASSED) display_colored("[PASSED] CACHE", "green");
+    else display_colored("[FAILED] ALU", "red");
+
+    ufp_rmask = 4'b0000;
+endtask
+
+task automatic test_dirty_miss_write(logic [31:0] addr, logic [31:0] expect_data, int line);
+    PASSED = 1'b1;
+    
+    if (addr[1:0] != 2'b00) begin
+    PASSED = 1'b0;
+    display_colored(" Addr not aligned", "red");
+    $fatal("%0t %s %0d: Addr not aligned", $time, `__FILE__, line, "red");
+    end
+    
+    ufp_addr = addr;
+    ufp_wmask = 4'b1111;
+    ufp_rmask = 4'b0000;
+    ufp_wdata = expect_data;
+
+    @(posedge clk);
+    if (ufp_resp) begin
+    PASSED = 1'b0;
+    display_colored("Should not hit", "red");
+    $fatal("%0t %s %0d: Should not hit", $time, `__FILE__, line, "red");
+     end
+    @(posedge clk);
+    @(negedge clk);
+    if (~mem_itf.write) begin 
+        PASSED = 1'b0;
+        display_colored("Should start write to main mem", "red");
+        $fatal("%0t %s %0d: Should start write to main mem", $time, `__FILE__, line, "red");
+    end 
+    if (mem_itf.read) begin 
+        PASSED = 1'b0;
+        display_colored("Should not read from main mem", "red");
+        $fatal("%0t %s %0d: Should not read from main mem", $time, `__FILE__, line, "red");
+    end 
+    begin 
+    @(posedge clk iff mem_itf.resp);
+    end
+    @(posedge clk);
+    if (~mem_itf.read) begin 
+        PASSED = 1'b0;
+        display_colored("Should start read from main mem", "red");
+        $fatal("%0t %s %0d: Should start read from main mem", $time, `__FILE__, line, "red");
+    end 
+    if (mem_itf.write) begin 
+        PASSED = 1'b0;
+        display_colored("Should not write to main mem", "red");
+        $fatal("%0t %s %0d: Should start write to main mem", $time, `__FILE__, line, "red");
+    end
+    begin 
+    @(posedge clk iff mem_itf.resp);
+    end
+    repeat (2) @(posedge clk);
+    if (~ufp_resp) begin 
+        PASSED = 1'b0;
+        display_colored("Should report to cpu", "red");
+        $fatal("%0t %s %0d: Should report to cpu", $time, `__FILE__, line, "red");
+    end 
+
+    if (PASSED) display_colored("[PASSED] CACHE", "green");
+    else display_colored("[FAILED] ALU", "red");
+
+    ufp_wmask = 4'b0000;
+    ufp_wdata = '0;
+endtask
+
+task automatic test_hit_write(logic [31:0] addr, logic [31:0] wdata, int line);
+ PASSED = 1'b1;
+    if (addr[1:0] != 2'b00) $fatal("%0t %s %0d: Addr not aligned", $time, `__FILE__, line);
+    ufp_addr = addr;
+    ufp_wmask = 4'b1111;
+    ufp_rmask = '0;
+    ufp_wdata = wdata;
+    repeat(2) @(posedge clk);
+   
+    if (~ufp_resp) begin
+    PASSED = 1'b0;
+     display_colored("Hit timeout", "red");
+     $fatal("%0t %s %0d: Hit timeout", $time, `__FILE__, line);
+     end
+     
+    if (mem_itf.read) begin
+     PASSED = 1'b0;
+    display_colored(" Should not read from mem", "red");
+     $fatal("%0t %s %0d: Should not read from mem", $time, `__FILE__, line);
+     end
+     
+    if (mem_itf.write) begin
+     PASSED = 1'b0;
+    display_colored(" Should not write to mem", "red");
+     $fatal("%0t %s %0d: Should not write to mem", $time, `__FILE__, line);
+     end
+
+    if (PASSED) display_colored("[PASSED] CACHE", "green");
+    else display_colored("[FAILED] CACHE", "red");
+   
+    ufp_wmask = '0;
+    ufp_wdata = '0;
+ 
 endtask
 
 
@@ -184,18 +395,31 @@ endtask
     initial begin
     do_reset();
    // verify_hits();
-    test_read_miss( 32'h00000000, 32'h00000000,`__LINE__ );
-    test_read_miss(32'h00000200, 32'h11111111, `__LINE__);
-    test_read_miss(32'h00000400, 32'h22222222, `__LINE__);
-    test_read_miss(32'h00000600, 32'h33333333, `__LINE__);
-    test_read_hit( 32'h00000000, 32'h00000000,`__LINE__ );
-    test_read_hit(32'h00000200, 32'h11111111, `__LINE__);
-    test_read_hit(32'h00000400, 32'h22222222, `__LINE__);
-    test_read_hit(32'h00000600, 32'h33333333, `__LINE__);
-    test_read_hit(32'h00000400, 32'h22222222, `__LINE__);
-    test_read_hit( 32'h00000000, 32'h00000000,`__LINE__ );
-    test_read_miss(32'h00000800, 32'h44444444, `__LINE__);
-    test_read_miss(32'h00000600, 32'h33333333, `__LINE__);
+    test_clean_miss_read( 32'h00000000, 32'h00000000,`__LINE__ );
+    test_clean_miss_write(32'h00000200, 32'h11111111, `__LINE__);
+    test_hit_read(32'h00000200, 32'h11111111, `__LINE__);
+    // test_clean_miss_read(32'h00000200, 32'h11111111, `__LINE__);
+    test_clean_miss_read(32'h00000400, 32'h22222222, `__LINE__);
+    test_clean_miss_read(32'h00000600, 32'h33333333, `__LINE__);
+    test_hit_read( 32'h00000000, 32'h00000000,`__LINE__ );
+    test_hit_read(32'h00000200, 32'h11111111, `__LINE__);
+    test_hit_read(32'h00000400, 32'h22222222, `__LINE__);
+    test_hit_read(32'h00000600, 32'h33333333, `__LINE__);
+    test_hit_read(32'h00000400, 32'h22222222, `__LINE__);
+    test_hit_read( 32'h00000000, 32'h00000000,`__LINE__ );
+    test_clean_miss_read(32'h00000800, 32'h44444444, `__LINE__);
+    test_dirty_miss_read(32'h00000600, 32'h33333333, `__LINE__);
+    test_hit_write(32'h00000600, 32'haaaaaaaa, `__LINE__);
+    test_hit_write(32'h00000400, 32'hbbbbbbbb, `__LINE__);
+    test_hit_write(32'h00000000, 32'hcccccccc, `__LINE__);
+    test_hit_write(32'h00000400, 32'hbbbbbbbb, `__LINE__);
+    test_hit_write(32'h00000800, 32'hdddddddd, `__LINE__);
+    test_dirty_miss_write(32'h00000200, 32'heeeeeeee, `__LINE__);
+    test_dirty_miss_read(32'h00000600, 32'haaaaaaaa, `__LINE__);
+    test_dirty_miss_read(32'h00000400, 32'hbbbbbbbb, `__LINE__);
+    test_dirty_miss_read(32'h00000000, 32'hcccccccc, `__LINE__);
+    test_dirty_miss_read(32'h00000800, 32'hdddddddd, `__LINE__);
+    test_hit_read(32'h00000600, 32'haaaaaaaa, `__LINE__);
 
     // test_read_hit(32'h00000800, 32'h11111111, `__LINE__);
     
